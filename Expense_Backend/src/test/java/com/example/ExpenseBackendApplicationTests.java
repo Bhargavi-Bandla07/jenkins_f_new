849a885f1bb0bfc8343dabe0fa9a4f3353d4e204
BHARGAVI <bhargavi07.b@gmail.com>
2025-10-17 08:29:12 +0530

    updated

---
+++ Expense_Backend/src/test/java/com/example/ExpenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest(classes = ExpenseBackendApplication.class)
+public class ExpenseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // simple smoke test to ensure Spring context starts
+    }
+}
