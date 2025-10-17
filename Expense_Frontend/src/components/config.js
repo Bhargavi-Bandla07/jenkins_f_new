7b302a9a6599e96cd516d838e10868ac93d684d9
BHARGAVI <bhargavi07.b@gmail.com>
2025-10-17 08:40:28 +0530

    updated

--- Expense_Frontend/src/components/config.js
+++ Expense_Frontend/src/components/config.js
@@ -1,5 +1,5 @@
 // src/config.js
 export default {
-  url: 'http://localhost:2003' // change to your backend base URL when ready
+  url: 'http://localhost:2030/Expense_backend1' // change to your backend base URL when ready
 };
 
